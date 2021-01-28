87a78b929b74157e652d11433d7d90b0ee20f56c
C5315733 <sindhu.ramagiri@sap.com>
2021-01-28 15:34:43 +0530

    doing mul hellocoding

--- src/main/java/HelloCoding.java
+++ src/main/java/HelloCoding.java
@@ -3,9 +3,12 @@ public class HelloCoding {
         int a= 20;
         int b= 30;
         int c= a+b;
-        System.out.println("addintion of a and b= " +c);
+        System.out.println("addition of a and b= " +c);
         int d = c-a;
         System.out.println("subtraction of c and a= " +d);
+        int e = c*d;
+        System.out.println("multiplication of c and d= " +e);
+
 
     }
 }
