3bf0545001789f54ffdbbcb16041d083e2c7f936
C5315733 <sindhu.ramagiri@sap.com>
2021-01-28 13:24:17 +0530

    adding hellocoding

---
+++ src/main/java/HelloCoding.java
@@ -0,0 +1,9 @@
+public class HelloCoding {
+    public static void main(String[] args){
+        int a= 20;
+        int b= 30;
+        int c= a+b;
+        System.out.println("addintion of a and b =" +c);
+
+    }
+}
